6ee01ba6a4c61cebe673f05b4a3d7978fe0eaf2a
Shivangi Singh <shivangisingh211@outlook.com>
2020-07-19 19:21:14 +0530

    COMMIT

--- WebLogDemo/Controllers/WeatherForecastController.cs
+++ WebLogDemo/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebLogDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "FREEZING", "BRACING", "CHILLY", "VOOLLLLLLLLLLL", "MILD", "WARM", "BALMY", "HOTT", "SWEKTERING", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
